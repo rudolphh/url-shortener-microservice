8a094a2531fd797b6a49ef5d03c92d46fdfdf54a
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-04-22 17:50:04 -0700

    local setup

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ var mongoose = require('mongoose');
 
 var cors = require('cors');
 
+var dotenv = require('dotenv');
+dotenv.load();
+
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
@@ -25,13 +28,13 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
+
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+var listener = app.listen(port, function () {
+  console.log('Node.js listening on ' + listener.address().port );
 });
