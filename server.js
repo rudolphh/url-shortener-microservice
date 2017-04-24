8ea59c20d025c740c97d4f1c480bbbc460e38984
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-04-23 22:13:48 -0700

    add function to find valid url in db and respond, or save in db and respond

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var dotenv = require('dotenv'); dotenv.load();
+var AutoIncrement = require('mongoose-sequence');
+
 
 var bodyParser = require('body-parser');
 
@@ -16,6 +18,7 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/
 mongoose.connect(process.env.MONGO_URI);
+mongoose.Promise = Promise;
 
 app.use(cors());
 
@@ -36,6 +39,50 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+///////////  our URL schema and model
+var Schema = mongoose.Schema;
+
+var urlSchema = new Schema({
+  original_url: {
+    type: String,
+    required: true
+  }
+});
+
+// we're using mongoose-sequence to make short_url auto-increment on every save
+urlSchema.plugin(AutoIncrement, {inc_field: 'short_url'});
+var URL = mongoose.model('URL', urlSchema);
+
+
+function urlFindOrSave(url, res){
+  URL.findOne({ original_url: url }, function(err, urlDoc){
+    if(err) { console.log(err); }
+    else { // success
+      if(urlDoc){ // found
+        res.json({
+          original_url: urlDoc.original_url,
+          short_url: urlDoc.short_url
+        });
+      } else { // its a new url so we're going to save in our db
+
+        var newURL = new URL({
+          original_url: url // note short_url is handled for us on save
+        });
+        newURL.save(function(err, newDoc){
+          if(err) { console.log(err); }
+          else {
+            // after we save we want to respond with json containing our new short_url index
+            res.json({
+              original_url: newDoc.original_url,
+              short_url: newDoc.short_url
+            });
+          }
+        });
+      }
+    }// end else (success)
+  })
+}
+
 const dns = require('dns');
 
 // our url shortener API endpoint
@@ -52,14 +99,9 @@ app.post('/api/shorturl/new', function(req, res) {
     dns.lookup(domain, function(err, addr, fam) {
       // if the domain isn't valid
       if(err) { res.json({ error: 'invalid URL' }); }
-      else {
-
-        // TODO add short_url key returned from mongoose save
-        
-        res.json({ original_url: url });
-      }
+      else { urlFindOrSave(url, res); }
     });
-  } else { // if not, fail
+  } else { // if no protocol, fail
     res.json({ error: 'invalid URL' });
   }
 
